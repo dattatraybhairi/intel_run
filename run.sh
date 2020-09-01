785c5344ccb5d0b81318d2e60115b461dbaeab5e
Dattatray Narhari Bhairi <dbhairi@gmail.com>
2020-09-01 13:04:39 +0530

    run to disable

---
+++ run.sh
@@ -0,0 +1,23 @@
+#!/bin/bash
+
+git clone https://github.com/dattatraybhairi/script-for-disable-enable-turbo-mode-in-intel
+
+cd script-for-disable-enable-turbo-mode-in-intel/
+
+cp turbo-boost.sh /home/vacus/Desktop
+
+cp mycronjob /home/vacus/Desktop
+
+sudo cp social.py /home/vacus/Desktop
+
+sudo chmod +x /home/vacus/Desktop/turbo-boost.sh
+
+sudo /home/vacus/Desktop/turbo-boost.sh disable
+
+sudo crontab mycronjob
+
+sudo systemctl restart broker
+
+sudo systemctl status broker
+
+echo "done"
